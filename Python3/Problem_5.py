1e4482da2ad390f324c2b38d1cc8c90ea550adac
Nimiras <pannkinn@gmail.com>
2022-05-04 22:40:59 +0200

    Commented Problem 5

--- Python3/Problem_5.py
+++ Python3/Problem_5.py
@@ -1,17 +1,18 @@
 ##############################
 #   Project Euler Problem 5  #
 ##############################
-
+# Checks linear every multiple of 20 if its divisible by all the numbers between 2 and 19
 x = 20
-z = 0
-while (z < 19):
-    z = 0
+counter = 0
+
+while (counter < 19):
+    counter = 0
     x += 20
     for i in range(2, 21):
         if x % i == 0:
-            z += 1
+            counter += 1
         else:
             break
-print(x)
 
-# Kleiner test
+
+print(x)
